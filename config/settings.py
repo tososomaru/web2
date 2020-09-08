65f0404b48d8c086f77da8ff72f4ddf485a96115
tososomaru <delatornikita@gmail.com>
2020-09-08 07:17:00 +0300

    change settings.py

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,22 @@ PROJECT_APPS = [
     'data',
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS + PACKAGE_APPS
 
 MIDDLEWARE = [
